a93c3aced40b3e44244392da983b3bb490e9411a
liaodalin19903 <liaodalin19903@126.com>
2024-08-02 08:26:02 +0800

    传入方法: 1）方法不带参数的

--- react-lml-comps/code-blockcontainer/src/App.tsx
+++ react-lml-comps/code-blockcontainer/src/App.tsx
@@ -6,9 +6,13 @@ import CodeBlockContainer from './component/code-blockcontainer'
 
 function App() {
 
+  const cb1 = () => {
+    console.log('子组件完成执行')
+  }
+
   return (
     <div className="App">
-      <CodeBlockContainer data1="112233"></CodeBlockContainer>
+      <CodeBlockContainer data1="112233" cb1={cb1}></CodeBlockContainer>
     </div>
   );
 }
--- react-lml-comps/code-blockcontainer/src/component/code-blockcontainer/index.tsx
+++ react-lml-comps/code-blockcontainer/src/component/code-blockcontainer/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 type CodeBlockContainerProps = {
-  data1: string 
+  data1: string,
+  cb1: () => void 
 }
 
 export default function index(props: CodeBlockContainerProps) {
 
   const clickButton = () => {
     console.log('输出父组件传入的数据：data1 = ', props.data1)
+
+    props.cb1()
   }
   
   return (
