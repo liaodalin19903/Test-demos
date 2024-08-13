b57b74a382a180df87bbed057e0f48eb04c90a23
markleo <liaodalin19903@126.com>
2024-08-13 12:52:43 +0800

    完成:forwardRef + useImperativeHandle 示例

--- react-test-demos/react-demo-05-forwardref/src/App.tsx
+++ react-test-demos/react-demo-05-forwardref/src/App.tsx
@@ -1,29 +1,58 @@
-import React, { forwardRef, LegacyRef, useRef } from 'react';
+import {forwardRef, useImperativeHandle, useRef } from 'react';
 
 import './App.css';
 
 //#region 子组件定义
-const ChildComp = forwardRef(( props, ref: LegacyRef<HTMLInputElement> ) => {
+interface InputRef {
+  focus: () => void;
+  testConsole: () => void;
+}
 
-  return <input value="我是子组件的input" ref={ref} />
-})
+type InputCompProp = {
+  placeholder: string 
+}
+ 
+const InputComp = forwardRef((props: InputCompProp, ref) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+ 
+  // 使用 useImperativeHandle 自定义 ref 暴露的方法
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    },
+
+    testConsole: () => {
+      console.log('子组件打印')
+    }
+  }));
+ 
+  return <>
+    <label>子组件</label>
+    <br></br>
+    <input ref={inputRef} {...props} />
+  </>
+});
+
 //#endregion
 
-function App() {
+const App = () => {
+  const inputRef = useRef<InputRef>(null);
 
-  const inpRef = useRef(null)
-
-  const focusFn = () => {
-    console.log(inpRef.current)
+  const clickHandler = () => {
+    if(inputRef.current) {
+      inputRef.current.focus(); // 使用 forwardRef 暴露的 ref 方法
+      inputRef.current.testConsole()
+    }
   }
-
-  return (
-    <div className="App">
-      <ChildComp ref={inpRef} />
-
-      <button onClick={focusFn}>点击打印当前inpuRef</button>
-    </div>
-  );
-}
+ 
+  return <>
+    <label>父组件</label>
+    <button onClick={clickHandler}>Focus Input</button>
+    <br></br>
+    <InputComp ref={inputRef} placeholder="Focus me" />;
+  </>
+};
 
 export default App;
