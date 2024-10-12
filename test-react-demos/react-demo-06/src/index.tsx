2c526943237c9fe912ac77ddba6f52d803207f7a
liaodalin19903 <liaodalin19903@126.com>
2024-10-12 19:22:31 +0800

    2024-10-12 19:22

--- test-react-demos/react-demo-06/src/index.tsx
+++ test-react-demos/react-demo-06/src/index.tsx
@@ -4,13 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
+export const queryClient = new QueryClient()
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+
+  <QueryClientProvider client={queryClient}>
+    <App/>
+    <ReactQueryDevtools initialIsOpen={false}/>
+  </QueryClientProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
