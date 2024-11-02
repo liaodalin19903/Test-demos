c12fd84660399df1d1efa4b150b902d4f59260d1
markleo <liaodalin19903@126.com>
2024-11-02 17:51:53 +0800

    2024.11.2

---
+++ test-react-demos/react-demo-06/src/test/crypto.test.js
@@ -0,0 +1,13 @@
+
+import crypto from '@mark-common-js/crypto-esm'
+
+
+import { it } from 'vitest'
+
+it('should work', () => {
+  // prints name of the test
+  console.log(123)
+  console.log(crypto.createHash)
+})
+
+
