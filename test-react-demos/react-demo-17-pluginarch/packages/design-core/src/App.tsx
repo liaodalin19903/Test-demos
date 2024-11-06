47a49a69b55499160462e611dcd21e43163f880f
markleo <liaodalin19903@126.com>
2024-11-06 18:11:40 +0800

    2024-11-06

---
+++ test-react-demos/react-demo-17-pluginarch/packages/design-core/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      1122
+    </>
+  )
+}
+
+export default App
---
+++ test-react-demos/react-demo-17-pluginarch/packages/design-core/src/config/plugins.ts
@@ -0,0 +1,11 @@
+// 插件注册 
+// 参考：tiny-engine/packages/design-core/config/addons.js
+// 标准规范：插件注册
+
+const plugins = {
+  toolbar: [], // 顶部工具栏
+  pluginbar: [], // 左侧插件栏
+  settingsbar: []  // 右侧设置栏
+}
+
+export default plugins 
