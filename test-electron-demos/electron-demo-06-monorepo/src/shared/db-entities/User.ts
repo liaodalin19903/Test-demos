f5bceb5d9637d32c43827267bf8931eb5edc6a9f
markleo <liaodalin19903@126.com>
2024-11-11 18:35:55 +0800

    2024-11-11

---
+++ test-electron-demos/electron-demo-06-monorepo/src/shared/db-entities/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  firstName: string
+
+  @Column()
+  lastName: string
+
+  @Column()
+  age: number
+
+  constructor(firstName: string = '', lastName: string = '', age: number = 0) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
