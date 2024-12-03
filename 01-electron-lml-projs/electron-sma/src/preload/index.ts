dc2e0c88a2be5b03c77191379b1dfec500fad04f
liaodalin19903 <liaodalin19903@126.com>
2024-12-03 13:11:00 +0800

    chore

--- 01-electron-lml-projs/electron-sma/src/preload/index.ts
+++ 01-electron-lml-projs/electron-sma/src/preload/index.ts
@@ -2,44 +2,6 @@ import { electronAPI } from '@electron-toolkit/preload'
 import {IElectronAPI, IpcRequest} from '@shared/@types'
 import {contextBridge, ipcRenderer} from "electron";
 
-import { Menu } from '@main/index';
-
-//#region custom titlebar
-import { Titlebar, TitlebarColor } from "custom-electron-titlebar"
-import path from 'path';
-import { MAIN_COLOUR } from '@shared/constants'
-
-const menuItems = ([
-  { label:'1' },
-  { label:'2' },
-  { label:'3' },
-])
-
-setTimeout(() => {
-  console.log('imported status: ', Menu, ipcRenderer)
-  }, 1000)
-
-window.addEventListener('DOMContentLoaded', () => {
-  // Title bar implementation
-  const options = {
-    // options
-    backgroundColor: TitlebarColor.fromHex(MAIN_COLOUR),
-    overflow: 'auto',
-    icon: path.join(__dirname, '../../resources/icon.png'),
-    tooltips: {
-      minimize: 'Minimize',
-      maximize: 'Maximize',
-      restoreDown: 'Restore',
-      close: 'Close'
-    },
-    shadow: false,
-    onlyShowMenubar: true,
-    //menu: Menu.buildFromTemplate(menuItems)
-  };
-  new Titlebar(options);
-});
-//#endregion
-
 // Custom APIs for renderer
 const api: IElectronAPI = {
   node: () => process.versions.node,
