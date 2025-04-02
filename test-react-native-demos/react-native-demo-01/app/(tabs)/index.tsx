caa4087d416d55a0a5a14224d26c06423341c602
markleo <liaodalin19903@126.com>
2025-04-02 19:25:07 +0800

    2025-04-02 19:25

--- test-react-native-demos/react-native-demo-01/app/(tabs)/index.tsx
+++ test-react-native-demos/react-native-demo-01/app/(tabs)/index.tsx
@@ -7,25 +7,54 @@ import {
 } from 'react-native';
 
 import { Avatar, Card,  Text } from 'react-native-paper';
+import { Appbar, Button, Divider, Drawer } from 'react-native-paper';
 
-import { Button } from 'react-native-paper';
 
 export default function HomeScreen() {
 
   //@ts-ignore
   const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
   
+  const handleClick = () => {
+    console.log('Pressed')
+
+  } 
+
   return (
+
     <View>
-      <Text>1122</Text>
-      <Text>1122</Text>
-      <Text>1122</Text>
-      <Text>1122</Text>
+      <Appbar.Header>
+        <Appbar.Content title="Home">
+        </Appbar.Content>
+        {/* 你可以在这里添加更多的 Appbar.Action 或其他组件 */}
+      </Appbar.Header>
       
-      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
-        Press me
-      </Button>
+      <View>
+        <View>
+          <Text>Test</Text>
+        </View>
+
+        <Divider/> 
+
+        <View>
+          <Button 
+            onPress={handleClick}
+          >点击按钮打开左侧栏</Button>
+        </View>
+
+        <View>
+          <Text>1122</Text>
+          <Text>1122</Text>
+          <Text>1122</Text>
+          <Text>1122</Text>
+          
+          <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </View>
+      </View>  
     </View>
+
   );
 }
 
