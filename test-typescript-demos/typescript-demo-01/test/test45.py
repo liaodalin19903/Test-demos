c1afb31f39c92be45c91deb7699de1163792cbf8
markleo <liaodalin19903@126.com>
2025-08-06 19:24:29 +0800

    2025-08-06 19:24

--- test-typescript-demos/typescript-demo-01/test/test45.py
+++ test-typescript-demos/typescript-demo-01/test/test45.py
@@ -2,7 +2,6 @@
 import oss2
 from oss2.credentials import EnvironmentVariableCredentialsProvider
 # 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-
 auth = oss2.Auth('---', '---') 
 
 # 填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
